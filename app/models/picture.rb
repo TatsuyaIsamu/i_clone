4b5156611d6c464082c82f39921bd76e10d999bb
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-05-24 14:15:38 +0900

    user model と picture model にバリデーションを追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,6 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorites_users, through: :favorites, source: :user
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
   has_many :favorites_pictures, through: :favorites, source: :picture
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 30 }
+
 end
